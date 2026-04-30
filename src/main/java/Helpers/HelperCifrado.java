4e5ae0c52846a594a5a69830342a925647acafd4
Physanto <hackmanu6@gmail.com>
2026-04-30 01:06:22 -0500

    Refactor: Se refactorizo parte del paquete de Helpers

--- src/main/java/Helpers/HelperCifrado.java
+++ src/main/java/Helpers/HelperCifrado.java
@@ -1,34 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Helpers;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
 /**
  *
  * @author Santiago Lopez
  */
+
+/**
+ * Clase la cual se encarga de hacer el cifrado de datos
+ */
 public class HelperCifrado {
-    
-      public static String CifrarSHA256(String mensaje)
-    {
-         try{
-            MessageDigest sha= MessageDigest.getInstance("SHA-256");
-            byte[] digest = sha.digest(mensaje.getBytes());
+
+    /**
+     * Cifra el mensaje pasado por argumento
+     * @param mensaje mensaje a encriptar
+     * @return el mensaje cifrado
+     */
+    public static String CifrarSHA256(String mensaje) {
+
+        try{
+            MessageDigest sha = MessageDigest.getInstance("SHA-256");
+            byte[] digests = sha.digest(mensaje.getBytes());
             StringBuilder hexString = new StringBuilder();
-            for(byte b: digest)
-            {
+
+            for(byte b : digests) {
                 hexString.append(String.format("%02x", b));
             }
-            
             return hexString.toString();
-            
-            
-        }catch( NoSuchAlgorithmException e)
-        {
+        }
+        catch( NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
