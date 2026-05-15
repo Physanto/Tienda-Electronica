a70b9f7f34c063eb8af92113071e428145fd3b74
Physanto <hackmanu6@gmail.com>
2026-05-14 19:44:32 -0500

    feature: se sigue trabajando en la implementacion del sistema de promociones para la tienda

--- src/main/java/GUI_Admin/VisualizarPersonasAdministrador.java
+++ src/main/java/GUI_Admin/VisualizarPersonasAdministrador.java
@@ -110,7 +110,7 @@ public class VisualizarPersonasAdministrador extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        lspersonasnube = HelperGestorBD.CargarPersonaGeneral();
+        //lspersonasnube = HelperGestorBD.CargarPersonaGeneral();
         //se cambia el metodo por el general, haciendo cambios
         //lspersonasnube = PersonaProvider.CargarInfoPersona();
 
